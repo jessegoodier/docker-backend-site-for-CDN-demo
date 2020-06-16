2523e010132bf584a86a09014324e45459185fd9
jesse goodier <jessegoodier@gmail.com>
2020-06-16 12:27:28 +0000

    initial load

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
+#remove cache directories before build
+sudo rm -rf var/cache/nginx/*
 docker build --tag=image-dump .
 
-if [  "$(docker ps -q -f name=image-dump)" ]; then
-    # cleanup
-    docker container rm -f image-dump;
+#check if container exists and remove if so
+if [ -n "$( docker container ls --all --quiet --filter name=image-dump )" ]; then
+  echo "Container exists, removing"
+  docker rm -f image-dump;
 fi
-# run your container
+
+#pass the ports and volumes
 docker run --restart unless-stopped --name=image-dump -d -p 80:80 -p 443:443 -p 8080:8080 -v $PWD/image-dump:/usr/share/nginx/html image-dump:latest
